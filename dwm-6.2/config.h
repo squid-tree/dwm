a245ab7c52b578a31669d063324251ebad540961
Finn Gould <90180547+squid-tree@users.noreply.github.com>
2021-09-19 10:54:58 +1000

    Changed Dmenu To Have Consistent Colouring

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -71,7 +71,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-h", "23",/*"-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4,*/ NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 // Adds optional support for flameshot gui (uncomment lines 81 & 90 to enable)
