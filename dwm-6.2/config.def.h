e40ad7f758724bd587dad236b476984034c01fd4
Finn Gould <90180547+squid-tree@users.noreply.github.com>
2021-09-21 13:53:39 +1000

    Added Icon Font To DWM

--- dwm-6.2/config.def.h
+++ dwm-6.2/config.def.h
@@ -8,7 +8,7 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "MesloLGLDZ-Regular.ttf:size=10" };
+static const char *fonts[]          = { "MesloLGLDZ-Regular.ttf:size=10", "icomoon:style=Regular:size=12" };
 static const char dmenufont[]       = "MesloLGLDZ-Regular.ttf:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
