bfb010a280fd6e52bc1df7d74932bfa054c6f3c6
squid-tree <acuriousquid@gmail.com>
2021-11-13 21:13:23 +1100

    Changed .xinitrc and refined

--- dwm-6.2/config.def.h
+++ dwm-6.2/config.def.h
@@ -7,6 +7,7 @@ static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int user_bh            = 30;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char *fonts[]          = { "MesloLGLDZ-Regular.ttf:size=10", "icomoon:style=Regular:size=12" };
 static const char dmenufont[]       = "MesloLGLDZ-Regular.ttf:size=10";
 static const char col_gray1[]       = "#222222";
--- dwm-bar/dwm_bar.sh
+++ dwm-bar/dwm_bar.sh
@@ -46,7 +46,7 @@ do
     #lowerbar=""
 
     
-    xsetroot -name "$(dwm_brightness)$(dwm_battery)$(dwm_pulse)$(date +" %a %d/%m/%y %H:%M")"
+    xsetroot -name "$(dwm_battery)$(dwm_pulse)$(date +" %a %d/%m/%y %H:%M")"
     
     # Uncomment the line below to enable the lowerbar 
 #    xsetroot -name "$upperbar;$lowerbar"
